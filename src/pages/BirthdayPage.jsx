941cfb7f9af054a9884d0ec3db09b8ee65becfa2
Muhammad Ahad <ahad97140@gmail.com>
2025-07-09 12:24:10 +0500

    Form changed

--- src/pages/BirthdayPage.jsx
+++ src/pages/BirthdayPage.jsx
@@ -15,23 +15,24 @@ export default function BirthdayPage() {
       </p>
       <div className="w-[368px] h-[212px] rounded-4xl bg-white mt-6">
         <div className="flex items-center justify-center h-full">
-          <div className="flex flex-col items-center gap-4">
+          <form className="flex flex-col items-center gap-4">
             <input
               type="date"
               className="w-full p-4 rounded-xl border-2 border-gray-200 focus:border-[#0984e3] bg-white text-gray-800 font-medium
               shadow-sm hover:shadow-md focus:outline-none focus:ring-2 focus:ring-[#0984e3]/20 transition-all duration-200 ease-in-out
-              placeholder-gray-400 cursor-pointer
-  "
+              placeholder-gray-400 cursor-pointer"
+              required
             />
             <button
               className="bg-gradient-to-r from-[#0984e3] to-[#06b6d4] text-white px-8 py-4 rounded-full text-lg font-semibold
               shadow-lg hover:shadow-xl transform hover:scale-105 transition-all duration-300 ease-in-out hover:bg-gradient-to-r
             hover:from-[#0873c7] hover:to-[#05a5c0] active:scale-95 focus:outline-none focus:ring-2 focus:ring-[#0984e3]
             focus:ring-opacity-50"
+              type="submit"
             >
               Continue
             </button>
-          </div>
+          </form>
         </div>
       </div>
     </section>
